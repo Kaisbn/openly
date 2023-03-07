5a2961b54c91bddb5c27ec9826e728f5a03851ba
Kais Bennaceur <bennaceur.kais@me.com>
2023-03-07 00:30:47 -0800

    Add basic tests

---
+++ tests/test_api.py
@@ -0,0 +1,32 @@
+from openly.api import APIRequestGenerator, APIRouteGenerator
+from openly.const import API_DEFAULT_BASE_URL
+
+def test_default_api_path_generator():
+    generator = APIRouteGenerator()
+
+    assert generator._BASE_URL == API_DEFAULT_BASE_URL
+
+    assert generator._BASE_LOGIN_URL == API_DEFAULT_BASE_URL
+
+def test_custom_api_path_generator():
+    base_url = "http://localhost:8080/"
+    generator = APIRouteGenerator(base_url)
+
+    assert generator._BASE_URL == base_url
+
+    assert generator._BASE_LOGIN_URL == base_url
+
+def test_default_api_request_generator():
+    generator = APIRequestGenerator()
+
+    assert generator.api_routes
+    assert generator.api_routes._BASE_URL == API_DEFAULT_BASE_URL
+    assert generator.api_routes._BASE_LOGIN_URL == API_DEFAULT_BASE_URL
+
+def test_custom_api_generator_generator():
+    base_url = "http://localhost:8080/"
+    generator = APIRequestGenerator(base_url)
+
+    assert generator.api_routes
+    assert generator.api_routes._BASE_URL == base_url
+    assert generator.api_routes._BASE_LOGIN_URL == base_url
